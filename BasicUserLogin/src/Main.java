22bb832b756d81d309f052dd1d9465f9b3ac7e94
Soner <114054564+SonerA1@users.noreply.github.com>
2022-12-20 20:39:38 +0300

    Add files via upload

---
+++ BasicUserLogin/src/Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String userName, password, pick;
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Kullanıcı Adınız : ");
+        userName = scan.nextLine();
+        System.out.print("Şifreniz : ");
+        password = scan.nextLine();
+
+        if (userName.equals("SonerA1") && password.equals("Java123")) {
+            System.out.println("Giriş Yaptınız!");
+        } else {
+            System.out.println("Bilgileriniz Yanlış. Lütfen tekrar deniyin...");
+            System.out.println("Şifrenizi sıfırlamak ister misiniz ?");
+        }
+        pick = scan.nextLine();
+
+        if (pick.equals("Yes")) {
+            System.out.println("Yeni şifrenizi giriniz.");
+            password = scan.nextLine();
+            System.out.println("Yeni şifrenizi tekrar giriniz.");
+            password = scan.nextLine();
+            if (password.equals("Java123")) {
+                System.out.println("Yeni şifreniz eski şifrenizle aynı olamaz! Tekrar Deneyin");
+            } else {
+                System.out.println("Şifreniz Başarıyla Oluşturuldu.Tekrar Giriş Yapınız.");
+            }
+
+        }
+    }
+}
+
