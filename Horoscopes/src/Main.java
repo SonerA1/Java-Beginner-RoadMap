73f323be24263631c245acb52bb6367d5fdea86f
Soner <114054564+SonerA1@users.noreply.github.com>
2022-12-25 16:13:58 +0300

    Java

--- Horoscopes/src/Main.java
+++ Horoscopes/src/Main.java
@@ -2,6 +2,23 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        
+        /*Koç Burcu : 21 Mart - 20 Nisan
+        Boğa Burcu : 21 Nisan - 21 Mayıs
+        İkizler Burcu : 22 Mayıs - 22 Haziran
+        Yengeç Burcu : 23 Haziran - 22 Temmuz
+        Aslan Burcu : 23 Temmuz - 22 Ağustos
+        Başak Burcu : 23 Ağustos - 22 Eylül
+        Terazi Burcu : 23 Eylül - 22 Ekim
+        Akrep Burcu : 23 Ekim - 21 Kasım
+        Yay Burcu : 22 Kasım - 21 Aralık
+        Oğlak Burcu : 22 Aralık - 21 Ocak
+        Kova Burcu : 22 Ocak - 19 Şubat
+        Balık Burcu : 20 Şubat - 20 Mart
+        Ödev
+        Aynı örneği switch-case kullanmadan yapınız.*/
+        
+        
         int month, day;
         String horoscope = "";
         boolean isError = false;
