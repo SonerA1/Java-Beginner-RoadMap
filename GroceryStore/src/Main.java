75feedc258135d3925cd55ed776db1d220c97c64
Soner <114054564+SonerA1@users.noreply.github.com>
2022-12-25 16:09:48 +0300

    Java

--- GroceryStore/src/Main.java
+++ GroceryStore/src/Main.java
@@ -2,6 +2,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        
+        /*Java ile kullanıcıların manavdan almış oldukları ürünlerin kilogram değerlerine göre toplam tutarını ekrana yazdıran programı yazın.
+        Meyveler ve KG Fiyatları
+        Armut : 2,14 TL
+        Elma : 3,67 TL
+        Domates : 1,11 TL
+        Muz: 0,95 TL
+        Patlıcan : 5,00 TL*/
+        
+        
         double armut = 2.14, elma = 3.67, domates = 1.11, muz = 0.95, patlican = 5.00;
         int aKg, eKg, dKg, mKg, pKg;
         Scanner scan = new Scanner(System.in);
