af5499fcfb50ac07473b0b89af3a167c438d68ad
Soner <114054564+SonerA1@users.noreply.github.com>
2022-12-25 16:08:52 +0300

    Java

--- BodyMassIndex/src/Main.java
+++ BodyMassIndex/src/Main.java
@@ -2,6 +2,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        
+        /*Vücut Kitle İndeksi Hesaplama
+        Java ile kullanıcıdan boy ve kilo değerlerini alıp bir değişkene atayın. Aşağıdaki formüle göre kullanıcının 
+        "Vücut Kitle İndeks" değerini hesaplayıp ekrana yazdırın.
+        Formül
+        Kilo (kg) / Boy(m) * Boy(m)*/
+        
         int weigth;
         double bodysize;
         double bmIndex;
