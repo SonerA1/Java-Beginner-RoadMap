6bebc25536a9d99fede6133a83736ca49cb8fb3d
Soner <114054564+SonerA1@users.noreply.github.com>
2022-12-25 16:12:32 +0300

    Java

--- SuggestEvent/src/Main.java
+++ SuggestEvent/src/Main.java
@@ -2,6 +2,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        
+        /*Koşullar :
+        Sıcaklık 5'dan küçük ise "Kayak" yapmayı öner.
+        Sıcaklık 5 ve 15 arasında ise "Sinema" etkinliğini öner.
+        Sıcaklık 15 ve 25 arasında ise "Piknik" etkinliğini öner.
+        Sıcaklık 25'ten büyük ise "Yüzme" etkinliğini öner.
+        Ödev
+        Aynı örnek üzerinden if koşulları başka hangi şekilde oluşturulabilirdi farklı çözüm yolları bulunuz.*/
+        
+        
         int heat;
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter temperature value : ");
