8fc5531074ec4d8c634194950a21867c2b41dc6e
Soner <114054564+SonerA1@users.noreply.github.com>
2022-12-25 16:16:04 +0300

    Java

--- ChineseZodiac/src/Main.java
+++ ChineseZodiac/src/Main.java
@@ -2,6 +2,25 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        /*Çin Zodyağı nedir?
+        4000 bin yıldır kullanılan bir astroloji çeşididir Çin astrolojisi ve insanları 12 değişik burç ve sembollerle tanımlar. 
+        Çin Zodyağı bu 12 burcun eşit aralıklarla(10 derece genişliğinde) sıralandığı bir hayvan halkasıdır ve yıldızlarla pek bir ilgisi yoktur.
+        Çin Zodyağı nasıl hesaplanır?
+        Çin zodyağı hesaplanırken kişinin doğum yılının 12 ile bölümünde kalana göre bulunur.
+        Doğum Tarihi %12 = 0 ➜ Maymun
+        Doğum Tarihi %12 = 1 ➜ Horoz
+        Doğum Tarihi %12 = 2 ➜ Köpek
+        Doğum Tarihi %12 = 3 ➜ Domuz
+        Doğum Tarihi %12 = 4 ➜ Fare
+        Doğum Tarihi %12 = 5 ➜ Öküz
+        Doğum Tarihi %12 = 6 ➜ Kaplan
+        Doğum Tarihi %12 = 7 ➜ Tavşan
+        Doğum Tarihi %12 = 8 ➜ Ejderha
+        Doğum Tarihi %12 = 9 ➜ Yılan
+        Doğum Tarihi %12 = 10 ➜ At
+        Doğum Tarihi %12 = 11 ➜ Koyun*/
+        
+        
         int year, calc;
 
         Scanner scan = new Scanner(System.in);
