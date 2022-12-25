3b4a3160bd9e2310757125fbd4740b2b67ddc3ad
Soner <114054564+SonerA1@users.noreply.github.com>
2022-12-25 16:07:20 +0300

    Java

--- Taximeter/src/Main.java
+++ Taximeter/src/Main.java
@@ -2,6 +2,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        
+        /*Taksimetre Programı
+        Java ile gidilen mesafeye (KM) göre taksimetre tutarını ekrana yazdıran programı yazın.
+
+        Taksimetre KM başına 2.20 TL tutmaktadır.
+        Minimum ödenecek tutar 20 TL'dir. 20 TL altında ki ücretlerde yine 20 TL alınacaktır.
+        Taksimetre açılış ücreti 10 TL'dir.*/
+        
         int km;
         double perKm = 2.20;
         double startPrice = 10.0;
