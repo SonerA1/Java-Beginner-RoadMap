e3afdcbaa949818c297966a8f485e04abb197e98
Soner <114054564+SonerA1@users.noreply.github.com>
2022-12-25 16:06:43 +0300

    Java

--- TriAngelHypotenuseFormul/src/Main.java
+++ TriAngelHypotenuseFormul/src/Main.java
@@ -2,6 +2,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
+        
+        /*Dik Üçgende Hipotenüs Bulan Program
+        Java ile kullanıcıdan dik kenarlarının uzunluğunu alan ve hipotenüsü hesaplayan programı yazın.
+         Ödev
+         Üç kenar uzunluğunu kullanıcıdan aldığınız üçgenin alanını hesaplayan programı yazınız.
+         Formül
+         Üç𝑔𝑒𝑛𝑖𝑛 ç𝑒𝑣𝑟𝑒𝑠𝑖 = 2𝑢
+         𝑢 = (a+b+c) / 2
+        Alan * Alan = 𝑢 * (𝑢 − 𝑎)* (𝑢 − 𝑏) * (𝑢 − 𝑐)*/
+        
         int aKisakenar,bUzunkenar;
         double cHipotenus;
         double yukseklik;
