3638f59a4066be722e7ea185458dca6edb2daf78
Soner <114054564+SonerA1@users.noreply.github.com>
2022-12-25 16:08:12 +0300

    Java

--- CircleAreaCircumference/src/Main.java
+++ CircleAreaCircumference/src/Main.java
@@ -2,6 +2,15 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        /*Dairenin Alanını ve Çevresini Hesaplayan Program
+        Java ile yarı çapını kullanıcıdan aldığınız dairenin alanını ve çevresini hesaplayan programı yazın.
+        Alan Formülü : π * r * r;
+        Çevre Formülü : 2 * π * r;
+        Ödev
+        Yarıçapı r, merkez açısının ölçüsü 𝛼 olan daire diliminin alanı bulan programı yazınız.
+        𝜋 sayısını = 3.14 alınız.
+        Formül : (𝜋 * (r*r) * 𝛼) / 360*/
+        
         int r;
         double pi = 3.14;
         double area;
